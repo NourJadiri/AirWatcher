28a72791054fee4ba4be2114fb3bf3e01e36591c
celiad113 <79829869+celiad113@users.noreply.github.com>
2023-05-09 15:20:06 +0200

    Update SuperUser.cpp

--- src/SuperUser.cpp
+++ src/SuperUser.cpp
@@ -2,4 +2,45 @@
 // Created by adrid on 09/05/2023.
 //
 
+//---------- Réalisation de la classe <SuperUser> (fichier SuperUser.cpp) ------------
+
+//---------------------------------------------------------------- INCLUDE
+
+//-------------------------------------------------------- Include système
+#include <iostream>
+using namespace std;
+
+//------------------------------------------------------ Include personnel
 #include "SuperUser.h"
+#include "User.h"
+
+
+//------------------------------------------------------------- Constantes
+
+//----------------------------------------------------------------- PUBLIC
+
+//----------------------------------------------------- Méthodes publiques
+
+//-------------------------------------------- Constructeurs - destructeur
+SuperUser::SuperUser ( const SuperUser & aSuperUser )
+{
+#ifdef MAP
+    cout << "Appel au constructeur de copie de <SuperUser>" << endl;
+#endif
+} //----- Fin de SuperUser (constructeur de copie)
+
+
+SuperUser::SuperUser ( )
+{
+#ifdef MAP
+    cout << "Appel au constructeur de <SuperUser>" << endl;
+#endif
+} //----- Fin de SuperUser
+
+SuperUser::~SuperUser ( )
+{
+#ifdef MAP
+    cout << "Appel au destructeur de <SuperUser>" << endl;
+#endif
+} //----- Fin de SuperUser
+
