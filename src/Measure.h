a3d38f7811c20eb2026f301b5659a0d3c77624e0
marieroulier <marie.roulier@insa-lyon.fr>
2023-05-09 16:23:48 +0200

    measure

---
+++ src/Measure.h
@@ -0,0 +1,49 @@
+/*************************************************************************
+                           Measure  -  description
+                             -------------------
+    début                : 09/05/2023
+    copyright            : (C) 2023 par Q41 : Adrien Morin, Isaline Foissey, Marie Roulier, Célia Djouadi et Nour ElJadiri
+*************************************************************************/
+
+//---------- Interface de la classe <Measure> (fichier Measure.h) ----------------
+#if ! defined ( MEASURE_H )
+#define MEASURE_H
+
+//--------------------------------------------------- Interfaces utilisées
+#include <string.h>
+#include <map>
+using namespace std;
+
+
+//------------------------------------------------------------- Constantes
+
+//------------------------------------------------------------------ Types
+
+//------------------------------------------------------------------------
+// Rôle de la classe <Measure>
+//
+//
+//
+//------------------------------------------------------------------------
+
+class Measure
+{
+//----------------------------------------------------------------- PUBLIC
+
+public:
+//----------------------------------------------------- Méthodes publiques
+
+
+//-------------------------------------------- Constructeurs - destructeur
+
+    Measure ( );
+
+
+    virtual ~Measure ( );
+
+
+};
+
+//-------------------------------- Autres définitions dépendantes de <Measure>
+
+#endif // MEASURE_H
