276d428111163356d77a5f04b637d23f6cb56517
celiad113 <79829869+celiad113@users.noreply.github.com>
2023-05-09 17:09:05 +0200

    Update Sensor.h

--- src/Sensor.h
+++ src/Sensor.h
@@ -5,28 +5,33 @@
     copyright            : (C) 2023 par Q41 : Adrien Morin, Isaline Foissey, Marie Roulier, Célia Djouadi et Nour ElJadiri
 *************************************************************************/
 
+//
+// Created by adrid on 09/05/2023.
+//
+
+#include <stdlib.h>
+#include <stdio.h>
+#ifndef AIRWATCHER_SENSOR_H
+#define AIRWATCHER_SENSOR_H
+
+
+#endif AIRWATCHER_SENSOR_H
+
 //---------- Interface de la classe <Sensor> (fichier Sensor.h) ----------------
-#if ! defined ( SENSOR_H )
+#if ! defined ( SENSOR_H)
 #define SENSOR_H
 
 //--------------------------------------------------- Interfaces utilisées
 #include <string>
 #include <map>
+#include <Coordinates.h>
 using namespace std;
-
-
-//------------------------------------------------------------- Constantes
-
-//------------------------------------------------------------------ Types
 
 //------------------------------------------------------------------------
 // Rôle de la classe <Sensor>
-//
-//
-//
 //------------------------------------------------------------------------
 
-class Sensor
+class Sensor 
 {
 //----------------------------------------------------------------- PUBLIC
 
@@ -34,15 +39,39 @@ public:
 //----------------------------------------------------- Méthodes publiques
 
 
+  //-----------------------------------------------------------------Getters
+    
+
+  //----------------------------------------------------------------Setters
+
+//------------------------------------------------- Surchge d'opérateurs
+    Sensor& operator = ( const Sensor & aSensor );
+     
+
 //-------------------------------------------- Constructeurs - destructeur
+ Sensor ( const Sensor & aSensor );
+  // Mode d'emploi (constructeur de copie) :
 
-    Sensor ( );
+  Sensor ( );
+  // Mode d'emploi (constructeur par défaut): 
+
+  Sensor::Sensor(string id, Coordinates coord, bool reliable)
+  // Mode d'emploi (Constructeur):
+  // Construit un capteur en attribuant à ses paramètres la valeur des paramètres en entrée. 
+
+  virtual ~Sensor ( );
+  //Destructeur de la class sensor. 
 
 
-    virtual ~Sensor ( );
+//------------------------------------------------------------------ PRIVE
 
+private:
 
+string idd;
+Coordinates coord;
+bool reliable;
 };
+
 
 //-------------------------------- Autres définitions dépendantes de <Sensor>
 
