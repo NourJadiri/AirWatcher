5c49cd33965f2fc1561983948e4dd6921bf5fe6f
AdrienMorin <adridu71170@gmail.com>
2023-05-23 14:52:47 +0200

    Sensor getters and setters

---
+++ src/Interface.cpp
@@ -0,0 +1,32 @@
+/*************************************************************************
+                           Interface  -  lancement du programme
+                             -------------------
+    début                : 09/05/2023
+    copyright            : (C) 2023 par Q41 : Adrien Morin, Isaline Foissey, Marie Roulier, Célia Djouadi et Nour ElJadiri
+*************************************************************************/
+
+//---------- Réalisation de la classe <Interface> (fichier Interface.cpp) ------------
+
+//---------------------------------------------------------------- INCLUDE
+
+//-------------------------------------------------------- Include système
+#include <iostream>
+using namespace std;
+
+//------------------------------------------------------ Include personnel
+#include "Interface.h"
+
+
+//----------------------------------------------------- Méthodes publiques
+
+//-------------------------------------------- Constructeurs - destructeur
+
+Interface::Interface ( )
+{
+} //----- Fin de Interface (constructeur par defaut)
+
+
+Interface::~Interface( )
+{
+}//------- Fin de ~Interface() (destructeur)
+
---
+++ src/Interface.h
@@ -0,0 +1,50 @@
+/*************************************************************************
+                           Interface  -  description
+                             -------------------
+    début                : 09/05/2023
+    copyright            : (C) 2023 par Q41 : Adrien Morin, Isaline Foissey, Marie Roulier, Célia Djouadi et Nour ElJadiri
+*************************************************************************/
+
+//---------- Interface de la classe <InitService> (fichier InitService.h) ----------------
+#if ! defined ( INTERFACE_H )
+#define INTERFACE_H
+
+//--------------------------------------------------- Interfaces utilisées
+#include <string>
+#include <map>
+using namespace std;
+
+
+//------------------------------------------------------------- Constantes
+
+//------------------------------------------------------------------ Types
+
+//------------------------------------------------------------------------
+// Rôle de la classe <Interface>
+//
+//
+//
+//------------------------------------------------------------------------
+
+class Interface
+{
+//----------------------------------------------------------------- PUBLIC
+
+public:
+//----------------------------------------------------- Méthodes publiques
+
+
+//-------------------------------------------- Constructeurs - destructeur
+
+    Interface ( );
+
+
+    virtual ~Interface ( );
+
+
+};
+
+//-------------------------------- Autres définitions dépendantes de <Interface>
+
+#endif // INTERFACE_H
+
